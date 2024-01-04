29560f2c80a8607fb376d53fa930ec871bc5aabd
dawit-gebeyehu <dawit.gebeyehu@testpro.io>
2024-01-03 22:07:57 -0600

    Update LoginTests.java

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -10,16 +10,22 @@ import java.time.Duration;
 
 public class LoginTests extends BaseTest {
     @Test
-    public void loginEmptyEmailPassword() {
+    public void loginEmptyEmailPasswordTest() {
+///Given I have set and opened the browser
 //      Added ChromeOptions argument below to fix websocket error
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--start-maximized");
 
         WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         String url = "https://qa.koel.app/";
+///When I open my URL
+        String url = "https://testpro.io/";
         driver.get(url);
+///And I enter valid login
+///And I enter valid password
         Assert.assertEquals(driver.getCurrentUrl(), url);
         driver.quit();
     }

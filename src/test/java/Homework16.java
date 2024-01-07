d1f85301210ff58a1dc03e9eb54ded651fa511ea
dawit-gebeyehu <dawit.gebeyehu@testpro.io>
2024-01-06 20:32:24 -0600

    Homework16

--- src/test/java/Homework16.java
+++ src/test/java/Homework16.java
@@ -8,37 +8,29 @@ import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-public class LoginTests extends BaseTest {
+public class Homework16 extends BaseTest {
     @Test
-    public void loginValidEmailPassword() {
-///Given I have set and opened the browser
-//      Added ChromeOptions argument below to fix websocket error
+    public void registrationNavigation() {
+    ///Given I have set and opened the browser
+    //Added ChromeOptions argument below to fix websocket error
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         options.addArguments("--start-maximized");
 
         WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-///When I opened my URL
+    ///When I opened my URL
         String url = "https://qa.koel.app/";
         driver.get(url);
-///And I Enter valid Login
-        WebElement loginInput = driver.findElement(By.cssSelector("[type='email']"));
-        loginInput.click();
-        loginInput.clear();
-        loginInput.sendKeys("dagebeyehu@gmail.com");
-///And I enter valid password
-        WebElement passwordInput = driver.findElement(By.cssSelector("[type='password']"));
-        passwordInput.click();
-        passwordInput.clear();
-        passwordInput.sendKeys("yozca5uQiLxb!");
-///And I click Login
-        WebElement loginBtn = driver.findElement(By.cssSelector("[type='submit']"));
-        loginBtn.click();
-//Then Avatar is Displayed
-        WebElement avatar = driver.findElement(By.cssSelector(".avatar"));
-        Assert.assertTrue(avatar.isDisplayed());
+
+        WebElement registrationLink = driver.findElement(By.cssSelector("[href='registration']"));
+        registrationLink.click();
+
+        String registrationURL = "https://qa.koel.app/registration";
+        Assert.assertEquals(driver.getCurrentUrl(), registrationURL);
+
         driver.quit();
+
 
     }
 

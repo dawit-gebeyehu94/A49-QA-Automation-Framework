27a357fc0eae8c51df3d3434e202cf9dfd7d3359
dawit-gebeyehu <dawit.gebeyehu@testpro.io>
2024-02-01 21:27:52 -0600

    HW25

---
+++ src/test/java/InvalidLoginTests.java
@@ -0,0 +1,25 @@
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import Pages.LoginPage;
+
+public class InvalidLoginTests extends BaseTest{
+    LoginPage loginPage;
+    @BeforeMethod
+    void setup(){
+        loginPage= new LoginPage(getThreadDriver());
+        getThreadDriver().get(url);
+    }
+    @Test
+    public void invalidPassword() throws InterruptedException {
+        loginPage.provideEmail("incorrectEmail").providePassword("incotrerctPwd").clickSubmit();
+        Thread.sleep(3000);
+    }
+    @Test
+    public void emptyPassword(){
+        loginPage.provideEmail("demo@class.com").providePassword("").clickSubmit();
+    }
+    @Test
+    public void emptyEmailPassword(){
+        loginPage.provideEmail("").providePassword("").clickSubmit();
+    }
+}
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,26 +1,16 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import Pages.LoginPage;
 
-import java.time.Duration;
-
-public class LoginTests extends BaseTest {
+public class LoginTests extends BaseTest{
+    LoginPage loginPage;
+    @BeforeMethod
+    void setup(){
+        loginPage= new LoginPage(getThreadDriver());
+        getThreadDriver().get(url);
+    }
     @Test
-    public void loginEmptyEmailPassword() {
-//      Added ChromeOptions argument below to fix websocket error
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-
-        WebDriver driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-        String url = "https://qa.koel.app/";
-        driver.get(url);
-        Assert.assertEquals(driver.getCurrentUrl(), url);
-        driver.quit();
+    public void validLoginPassword(){
+        loginPage.loginCorrectCred();
     }
 }

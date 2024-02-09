0d0de0210f9ca352309a06eff5108116d983b906
dawit-gebeyehu <dawit.gebeyehu@testpro.io>
2024-02-09 12:00:14 -0600

    HW29

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,16 +1,16 @@
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pagefactory.LoginPage;
-
 public class LoginTests extends BaseTest{
-    LoginPage loginPage;
     @Test
     public void validPassword() {
+        LoginPage loginPage = new LoginPage(getDriver());
         loginPage.loginCorrectCred();
         Assert.assertEquals(driver.getCurrentUrl(), url);
     }
     @Test
     public void validLoginPassword(){
+        LoginPage loginPage = new LoginPage(getDriver());
         loginPage.loginCorrectCred();
         Assert.assertEquals(driver.getCurrentUrl(), url);
     }

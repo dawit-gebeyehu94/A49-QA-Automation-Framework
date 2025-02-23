6a41ba5a98283ac5b4d66f42d20974c2f088ed24
dawit-gebeyehu <dawit.gebeyehu@testpro.io>
2025-02-22 21:48:50 -0600

    HW18

---
+++ src/test/java/HW18.java
@@ -0,0 +1,30 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class HW18 extends BaseTest{
+    @Test
+    public void playSong() {
+        provideEmail("dawit.gebeyehu@testpro.io");
+        providePassword("yozca5uQiLxb!");
+        clickSubmit();
+        clickPlay();
+        Assert.assertTrue(isSongPlaying());
+
+    }
+
+    public boolean isSongPlaying() {
+        WebElement soundbar = driver.findElement(By.xpath("//div[@data-testid='sound-bar-play']"));
+        return soundbar.isDisplayed();
+
+    }
+
+    public void clickPlay() {
+        WebElement playNextButton = driver.findElement(By.xpath("//i[@data-testid='play-next-btn']"));
+        WebElement playButton = driver.findElement(By.xpath("//span[@data-testid='play-btn"));
+        playNextButton.click();
+        playButton.click();
+
+    }
+}

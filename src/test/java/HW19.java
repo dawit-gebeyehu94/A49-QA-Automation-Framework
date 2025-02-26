92250c075142a575819530846d65fddd575df407
dawit-gebeyehu <dawit.gebeyehu@testpro.io>
2025-02-25 20:53:14 -0600

    HW19Upt

---
+++ src/test/java/HW19.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.Test;
+
+public class HW19 extends BaseTest{
+
+    @Test
+    public void deletePlaylist(){
+        navigateToPage();
+        provideEmail("dawit.gebeyehu@testpro.io");
+        providePassword("yozca5uQiLxb!");
+        clickSubmit();
+
+
+
+    }
+}
